3cf209944e84b37e0687e1e4edb596e08dc06bf6
zxhbin <zxhbin@163.com>
2015-08-03 13:52:00 +0800

    zhouxinghai

---
+++ src/main/java/org/dl/tengrui/resource/UserResource.java
@@ -0,0 +1,11 @@
+package org.dl.tengrui.resource;
+
+public class UserResource {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test");
+	}
+
+}
