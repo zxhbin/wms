cc9422e2c0e5633313ff2b71f8624272287d48cc
zxhbin <zxhbin@163.com>
2015-08-11 19:22:36 +0800

    zhouxinghai

--- src/main/webapp/js/warehouse.js
+++ src/main/webapp/js/warehouse.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 	
 	$('#loginName').text(sessionStorage.getItem("USER_NAME"));
 	
+	$('.js_sub_menu').click(function(e) {		
+		//清除所有的选中状态
+		$('.js_sub_menu').parent().parent().removeClass('active');
+		$('.js_sub_menu').removeClass('active');
+		//选中状态
+		$(this).parent().parent().addClass('active');
+		$(this).addClass('active');
+	});
+	
 });
